2ae78b498e1cbcf4a444451fbe4acca39858d6e5
Jeffrey Zou <jeffreyzou13@gmail.com>
2022-08-02 14:40:39 -0400

    added home page, added font awesome icons

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,14 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+export default function Header() {
+  return (
+    <header>
+      <p className="header-name"> Jeffrey Zou </p>
+      <div className="menu">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="/resume">Resume</a>
+      </div>
+      <hr className="header-line" />
+    </header>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,39 @@
 import Head from 'next/head'
 import Header from '@components/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import Footer from '@components/Footer'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Jeffrey Zou</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
+      <div className="main-container">
+        <Header />
+        
+        <h1>Hello there!</h1>
+        <p>Welcome to my page. Come get to know a little about me and take a look at my projects</p>
+        <p>Check out some of my social links below:</p>
+        <a href="https://www.linkedin.com/in/jeffreyzou13" className="social-button linkedin" target="_blank">
+          <FontAwesomeIcon className="social-icon" icon={faLinkedin} />
+          Linkedin
+        </a>
+        <a href="https://github.com/JeffreyZou13" className="social-button github" target="_blank">
+          <FontAwesomeIcon className="social-icon" icon={faGithub} />
+          Github
+        </a>
+        <a href="mailto:jeffreyzou13@gmail.com" className="social-button email">
+          <FontAwesomeIcon className="social-icon" icon={faEnvelope} />
+          Email
+        </a>
+        
+        <Footer />
+      </div>
     </div>
   )
 }
